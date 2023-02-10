0479dd03ad65c5aec0249641c6674654c1bdede0
Ayala Bartal <ayalabartal@gmail.com>
2023-02-10 16:04:31 -0500

    Replace path with constants in ClassifierControllerTest

--- code/src/test/java/orbartal/interview/gosecure/ClassifierControllerTest.java
+++ code/src/test/java/orbartal/interview/gosecure/ClassifierControllerTest.java
@@ -24,6 +24,11 @@ public class ClassifierControllerTest {
 	private static final int MB100 = 100 * MB1;
 	private static final int GB1 =  1000 * MB1;
 
+	private static final String PATH_TO_SMALL_FILE = Configuration.get().getPathToSmallFile();
+	private static final String PATH_TO_MEDIUM_FILE = Configuration.get().getPathToMediumFile();
+	private static final String PATH_TO_LARGE_FILE = Configuration.get().getPathToLargeFile();
+	private static final String PATH_TO_HUGE_FILE = Configuration.get().getPathToHugeFile();
+
 	private final ClassifierTesterAssist assist = new ClassifierTesterAssist();
 
 	public ClassifierControllerTest() {
@@ -32,86 +37,74 @@ public class ClassifierControllerTest {
 
 	@Test
 	public void testOneSmallFile() {
-		String path = Configuration.get().getPathToSmallFile();
-		assist.testClassifierCountWordsOnWeekDaysAsWords(path, KB1);
+		assist.testClassifierCountWordsOnWeekDaysAsWords(PATH_TO_SMALL_FILE, KB1);
 	}
 
 	@Test
 	public void testOneMediumFile() {
-		String path = Configuration.get().getPathToMediumFile();
-		assist.testClassifierCountWordsOnWeekDaysAsWords(path, MB1);
+		assist.testClassifierCountWordsOnWeekDaysAsWords(PATH_TO_MEDIUM_FILE, MB1);
 	}
 
 	@Test
 	public void testOneLargeFile() {
-		String path = Configuration.get().getPathToLargeFile();
-		assist.testClassifierCountWordsOnWeekDaysAsWords(path, MB100);
+		assist.testClassifierCountWordsOnWeekDaysAsWords(PATH_TO_LARGE_FILE, MB100);
 	}
 
 	@Test
 	public void testOneHugeFile() throws FileNotFoundException, UnsupportedEncodingException {
-		String path = Configuration.get().getPathToHugeFile();
-		assist.testClassifierCountWordsOnWeekDaysAsWords(path, GB1);
+		assist.testClassifierCountWordsOnWeekDaysAsWords(PATH_TO_HUGE_FILE, GB1);
 	}
 	
 	@Test
 	public void testManySmallFilesInSequence() {
-		String basePath = Configuration.get().getPathToSmallFile();
 		for (int i=0; i<100; i++) {
-			assist.testOneFile(basePath, i);
+			assist.testOneFile(PATH_TO_SMALL_FILE, i);
 		}
 	}
 
 	@Test
 	public void testManySmallFilesInParallel () throws Exception {
-		String basePath = Configuration.get().getPathToSmallFile();
 		ForkJoinPool myPool = new ForkJoinPool(20);
 		myPool.submit(() ->
-			IntStream.range(1, 100).boxed().parallel().forEach(i->assist.testOneFile(basePath, i))
+			IntStream.range(1, 100).boxed().parallel().forEach(i->assist.testOneFile(PATH_TO_SMALL_FILE, i))
 		).get();
 	}
 	
 	@Test
 	public void testOneSmallFileWithManyWords () throws Exception {
 		List<String> words = readRandomWords(100);
-		String path = Configuration.get().getPathToSmallFile();
-		assist.testClassifierCountWordsWithWordList(path, KB1, words);
+		assist.testClassifierCountWordsWithWordList(PATH_TO_SMALL_FILE, KB1, words);
 	}
 	
 	@Test
 	public void testOneLargeFileWithManyWords () throws Exception {
 		List<String> words = readRandomWords(1000 * 10);
-		String path = Configuration.get().getPathToLargeFile();
-		assist.testClassifierCountWordsWithWordList(path, MB100, words);
+		assist.testClassifierCountWordsWithWordList(PATH_TO_LARGE_FILE, MB100, words);
 	}
 
 	//Oxford Dictionary define 273,000 words
 	@Test
 	public void testOneHugeFileWithManyWords () throws Exception {
 		List<String> words = readRandomWords(1000 * 273);
-		String path = Configuration.get().getPathToLargeFile();
-		assist.testClassifierCountWordsWithWordList(path, GB1, words);
+		assist.testClassifierCountWordsWithWordList(PATH_TO_LARGE_FILE, GB1, words);
 	}
 
 	@Test
 	public void testOneSmallFileWithNotCaseSensitive () {
 		List<String> words = Arrays.asList("Abcd", "aBcd", "abcd", "ABCD");
-		String path = Configuration.get().getPathToSmallFile();
-		assist.testClassifierCountWordsWithWordList(path, KB1, words);
+		assist.testClassifierCountWordsWithWordList(PATH_TO_SMALL_FILE, KB1, words);
 	}
 	
 	@Test
 	public void testOneSmallFileWithFilterByWordSize () {
 		List<String> words = Arrays.asList("abcd", "abc", "a", "dc");
-		String path = Configuration.get().getPathToSmallFile();
-		assist.testClassifierCountWordsWithWordList(path, KB1, words);
+		assist.testClassifierCountWordsWithWordList(PATH_TO_SMALL_FILE, KB1, words);
 	}
 	
 	@Test
 	public void testOneSmallFileWithFilterBlancks () {
 		List<String> words = Arrays.asList("abcd", " ", "  ");
-		String path = Configuration.get().getPathToSmallFile();
-		assist.testClassifierCountWordsWithWordList(path, KB1, words);
+		assist.testClassifierCountWordsWithWordList(PATH_TO_SMALL_FILE, KB1, words);
 	}
 
 	private List<String> readRandomWords(int size) {
